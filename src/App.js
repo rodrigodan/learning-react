8a97b7146731318cba0d4413e94c6a06ca9aafdb
rodrigogoncalves123 <rodrigodaniel@cc.ci.ufpb.br>
2019-09-24 23:13:18 -0300

    update class component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first react project!
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+
+    /* state is a component object*/
+    this.state = {
+      string: 'Hello Rodrigo'
+    }
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+
+          {/* setState is a component function */}
+          {/* setState comes with all properties we're gonna change in our state */}
+          <button onClick = {() => this.setState({ string: 'Hello Rodrigo Gonçalves Daniel' })}>
+            Change the text
+          </button>
+
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
