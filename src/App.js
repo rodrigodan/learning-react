61f97aad176234c833fc96c7a47a57c08231cd6a
rodrigogoncalves123 <rodrigodaniel@cc.ci.ufpb.br>
2019-09-29 22:39:18 -0300

    update Card Component

--- src/App.js
+++ src/App.js
@@ -12,20 +12,7 @@ class App extends Component{
 
     /* state is a component object*/
     this.state = {
-     monsters: [
-      {
-        name: 'Frankenstein',
-        id: 'asc1'
-      },
-      {
-        name: 'Dracula',
-        id: 'asc2'
-      },
-      {
-        name: 'zombie',
-        id: 'asc3'
-      }
-    ]
+     monsters: []
    }
   }
 
@@ -48,17 +35,8 @@ class App extends Component{
       in the card list */}
       {/* also, it has some other atrbute called children  */}
       {/* this children is any element inside the tag CardList */}
-      <CardList name = "Rodrigo" age = "24">
-         {/* react needs to know what elements it need to update */}
-        {/* if one of this elements in our array has a value that changes*/}
-        {/* without that that, our jsx would need to re render everything every time */}
-        {/* And that is heaaaavy */}
-        {/* then, with monsters.key inside h1 */}
-        {/* I can know what element changes and update just that instead of thousands */}
-        {  
-          this.state.monsters.map(monsters => (
-            <h1 key = {monsters.id}> {monsters.name} </h1>
-          )) 
+      <CardList monsters = {this.state.monsters}>
+
         } 
       </CardList>
 
--- src/components/card-list/card-list.components.jsx
+++ src/components/card-list/card-list.components.jsx
@@ -2,8 +2,31 @@ import React from 'react';
 
 import './card-list.styles.css';
 
+import {Card} from '../card/card.component';
+
 // props 
-export const CardList = props => {
-    console.log(props);
-return (<div className = "card-list">{props.children}</div>);
-}
+export const CardList = props => (
+
+    /* moved this from the last commit that was in the app, cause rendering card-list is not
+    responsibility of App, but the card-list component:
+    */
+
+    // however our card list component should not think about details of the elements and images
+
+    // the reponsibility of it is another component. This one is just about thinking what to add
+
+
+
+       /* react needs to know what elements it need to update */
+        /* if one of this elements in our array has a value that changes */
+        /* without that that, our jsx would need to re render everything every time */
+        /* And that is heaaaavy */
+        /* then, with monsters.key inside h1 */
+        /* I can know what element changes and update just that instead of thousands */
+
+    <div className = "card-list">
+        {props.monsters.map(monster => (
+            <Card key = {monster.id} monster = {monster} />
+          ))}
+    </div>
+);
