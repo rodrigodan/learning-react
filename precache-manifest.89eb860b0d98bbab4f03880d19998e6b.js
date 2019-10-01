95c276ecde88bfd10a8469e73a4ec1a1a2099dea
rodrigogoncalves123 <rodrigodaniel@cc.ci.ufpb.br>
2019-10-01 01:24:05 -0300

    Updates

---
+++ precache-manifest.89eb860b0d98bbab4f03880d19998e6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1f4c07f14635ec041051a3f16bd81c",
+    "url": "/learning-react/index.html"
+  },
+  {
+    "revision": "636691561b00622a3aaf",
+    "url": "/learning-react/static/css/main.91928431.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/learning-react/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "636691561b00622a3aaf",
+    "url": "/learning-react/static/js/main.9028aefb.chunk.js"
+  },
+  {
+    "revision": "a5543295b4fc6e160da2",
+    "url": "/learning-react/static/js/runtime-main.af272da6.js"
+  }
+]);
